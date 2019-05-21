a8e2bf1dd8321fe8b5b934f44391e546fe9f3dac
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-05-21 11:16:51 +0100

    thermostat temperature increases with an up function

--- test/thermostat.spec.js
+++ test/thermostat.spec.js
@@ -1,10 +1,6 @@
 import { Thermostat } from '../src/thermostat';
 
-
-
 describe("Thermostat", () => {
-
-  
 
   it('thermostat temperature starts at 20 degrees', () => {
     const thermostat = new Thermostat();
