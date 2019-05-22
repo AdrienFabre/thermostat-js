0e2e4c21754d2c91fc96bee9bafc6385364d05e0
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-05-22 11:41:27 +0100

    thermostat temperature can not be higher than 25 degrees when power saving mode is ON

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,10 +2,13 @@ export class Thermostat {
 
   constructor() { 
     this.temperature = 20;
+    this.powerSavingMode = true;
   }
 
   up() {
+    if (this.temperature < 25) {
     this.temperature += 1;
+    }
   }
 
   down() {
--- test/thermostat.spec.js
+++ test/thermostat.spec.js
@@ -8,25 +8,34 @@ describe("Thermostat", () => {
     thermostat = new Thermostat();
   })
 
-  it('thermostat temperature starts at 20 degrees', () => {
-    expect(thermostat.temperature).toBe(20);
-  });
+  describe("Power saving mode is ON", () => {
 
-  it('thermostat temperature increases with an up function', () => {
-    thermostat.up()
-    thermostat.up()
-    expect(thermostat.temperature).toBe(22);
-  });
+    it('thermostat temperature starts at 20 degrees', () => {
+      expect(thermostat.temperature).toBe(20);
+    });
 
-  it('thermostat temperature decreases with a down function', () => {
-    thermostat.down()
-    thermostat.down()
-    expect(thermostat.temperature).toBe(18);
-  });
+    it('thermostat temperature increases with an up function', () => {
+      thermostat.up()
+      thermostat.up()
+      expect(thermostat.temperature).toBe(22);
+    });
 
-  it('thermostat temperature can not be lower than 10 degrees', () => {
-    for (let i=0; i<12; i++){ thermostat.down()}
-    expect(thermostat.temperature).toBe(10);
+    it('thermostat temperature decreases with a down function', () => {
+      thermostat.down()
+      thermostat.down()
+      expect(thermostat.temperature).toBe(18);
+    });
+
+    it('thermostat temperature can not be lower than 10 degrees', () => {
+      for (let i=0; i<12; i++){ thermostat.down()}
+      expect(thermostat.temperature).toBe(10);
+    });
+
+    it('thermostat temperature can not be higher than 25 degrees', () => {
+      for (let i=0; i<7; i++){ thermostat.up()}
+      expect(thermostat.temperature).toBe(25);
+    });
+
   });
 
 })
