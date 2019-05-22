20a032fdf04e25c2730fbc72154188842ff0e8bf
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-05-22 13:47:42 +0100

    refactor

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,28 +1,54 @@
 export class Thermostat {
-
-  constructor() { 
+  constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
+    this.MAXIMUM_TEMPERATURE_PSM_ON = 25;
+    this.MAXIMUM_TEMPERATURE_PSM_OFF = 32;
+    this.MEDIUM_USAGE_LIMIT = 18;
+    this.HIGH_USAGE_LIMIT = 25;
     this.temperature = 20;
     this.powerSavingMode = true;
   }
 
   up() {
-    if (this.powerSavingMode == false && this.temperature < 32) {
+    if (
+      this.powerSavingMode == false &&
+      this.temperature < this.MAXIMUM_TEMPERATURE_PSM_OFF
+    ) {
       this.temperature += 1;
-      } else if (this.temperature < 25) {
-    this.temperature += 1;
+    } else if (this.temperature < this.MAXIMUM_TEMPERATURE_PSM_ON) {
+      this.temperature += 1;
     }
   }
 
   down() {
-    if (this.temperature > 10) {
-    this.temperature -= 1;
+    if (this.temperature > this.MINIMUM_TEMPERATURE) {
+      this.temperature -= 1;
     }
   }
 
   reset() {
-    this.temperature = 20;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
   }
 
+  powerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
 
+  powerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+
+  currentEnergyUsage() {
+    if (this.temperature < this.MEDIUM_USAGE_LIMIT) {
+      return "low-usage";
+    } else if (
+      this.MEDIUM_USAGE_LIMIT < this.temperature &&
+      this.temperature < this.HIGH_USAGE_LIMIT
+    ) {
+      return "medium-usage";
+    } else {
+      return "high-usage";
+    }
+  }
 }
-
--- test/thermostat.spec.js
+++ test/thermostat.spec.js
@@ -1,64 +1,89 @@
-import { Thermostat } from '../src/thermostat';
+import { Thermostat } from "../src/thermostat";
 
 describe("Thermostat", () => {
-
   let thermostat;
 
   beforeEach(() => {
     thermostat = new Thermostat();
-  })
+  });
+
+  const change_temperature = (direction, times) => {
+    for (let i = 0; i < times; i++) {
+      if (direction == "up") {
+        thermostat.up();
+      } else if (direction == "down") {
+        thermostat.down();
+      }
+    }
+  };
+
+  it("can switch between power saving mode ON and OFF", () => {
+    thermostat.powerSavingModeOff();
+    expect(thermostat.powerSavingMode).toBe(false);
+    thermostat.powerSavingModeOn();
+    expect(thermostat.powerSavingMode).toBe(true);
+  });
+
+  it("returns current energy usage: low-usage", () => {
+    thermostat.temperature = 17;
+    expect(thermostat.currentEnergyUsage()).toBe("low-usage");
+  });
+
+  it("returns current energy usage: medium-usage", () => {
+    thermostat.temperature = 19;
+    expect(thermostat.currentEnergyUsage()).toBe("medium-usage");
+  });
+
+  it("returns current energy usage: high-usage", () => {
+    thermostat.temperature = 26;
+    expect(thermostat.currentEnergyUsage()).toBe("high-usage");
+  });
 
   describe("Power saving mode is ON", () => {
-
-    it('thermostat temperature starts at 20 degrees', () => {
+    it("temperature starts at 20 degrees", () => {
       expect(thermostat.temperature).toBe(20);
     });
 
-    it('thermostat temperature increases with an up function', () => {
-      thermostat.up()
-      thermostat.up()
+    it("temperature increases with an up function", () => {
+      change_temperature("up", 2);
       expect(thermostat.temperature).toBe(22);
     });
 
-    it('thermostat temperature decreases with a down function', () => {
-      thermostat.down()
-      thermostat.down()
+    it("temperature decreases with a down function", () => {
+      change_temperature("down", 2);
       expect(thermostat.temperature).toBe(18);
     });
 
-    it('thermostat temperature can not be lower than 10 degrees', () => {
-      for (let i=0; i<12; i++){ thermostat.down()}
+    it("temperature can not be lower than 10 degrees", () => {
+      change_temperature("down", 12);
       expect(thermostat.temperature).toBe(10);
     });
 
-   it('thermostat temperature can not be higher than 25 degrees', () => {
-      for (let i=0; i<7; i++){ thermostat.up()}
+    it("temperature can not be higher than 25 degrees", () => {
+      change_temperature("up", 7);
       expect(thermostat.temperature).toBe(25);
     });
 
-    it('thermostat temperature comes back to 20 degree when using reset', () => {
-      for (let i=0; i<4; i++){ thermostat.up()}
-      thermostat.reset()
+    it("temperature comes back to 20 degree when using reset", () => {
+      change_temperature("up", 4);
+      thermostat.reset();
       expect(thermostat.temperature).toBe(20);
     });
-
   });
 
   describe("Power saving mode is OFF", () => {
-
     beforeEach(() => {
-      thermostat.powerSavingMode = false;
+      thermostat.powerSavingModeOff();
     });
-    it('thermostat temperature can be higher than 25 degrees', () => {
-      for (let i=0; i<10; i++){ thermostat.up()}
+
+    it("temperature can be higher than 25 degrees", () => {
+      change_temperature("up", 10);
       expect(thermostat.temperature).toBe(30);
     });
 
-    it('thermostat temperature can not be higher than 32 degrees', () => {
-      for (let i=0; i<15; i++){ thermostat.up()}
+    it("temperature can not be higher than 32 degrees", () => {
+      change_temperature("up", 15);
       expect(thermostat.temperature).toBe(32);
     });
-
   });
-
-})
+});
